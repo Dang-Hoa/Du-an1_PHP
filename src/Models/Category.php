da3d0d749de7d2b2615e5b0b0f5692cfb662d5bd
Dang Hoa <hoadvph28719@fpt.edu.vn>
2023-11-21 07:37:29 +0700

    fff

---
+++ src/Models/Category.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ductong\BaseMvc\Models;
+
+use Ductong\BaseMvc\Model;
+
+class Category extends Model
+{
+    protected $table = 'categories';
+    protected $columns = [
+        'name_category',
+    ];
+}
